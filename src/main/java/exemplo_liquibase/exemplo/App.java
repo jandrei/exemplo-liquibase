e8241634fe233b6acddce9a67911a116ea96accc
jandrei <jandrei.cm@gmail.com>
2015-06-23 17:41:30 -0300

    commit inicial

---
+++ src/main/java/exemplo_liquibase/exemplo/App.java
@@ -0,0 +1,13 @@
+package exemplo_liquibase.exemplo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
